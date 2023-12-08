cc85e894ae303de2c0ee28d4e2af40683f7197e6
Braulioffm <braulioffm@gmail.com>
2023-12-07 22:27:51 -0600

    build en carpeta docs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vueJSTodo/',
+  // base: '/vueJSTodo/',
   plugins: [vue()],
   server: { hrml: false },
 
